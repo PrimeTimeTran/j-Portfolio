9139cd6465a3b9e82cb227dd5c9cfa9115f650db
Loi Van Tran <datloiboi@gmail.com>
2020-09-21 14:29:04 +0700

    Add google analytics

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ const queryString = window.location.search;
 if (queryString) {
   const cat = queryString.split('=')[1]
   const posts = document.querySelectorAll('.portfolio-item')
-  for (post of posts) {
-    console.log(post.classList[2] === cat)
+  for (const post of posts) {
     if (post.classList[2] !== cat) {
       post.remove()
     }
